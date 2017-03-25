7ec3f9f4ac72763484d84d1d8770879f44878c45
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-03-25 17:18:14 +0000

    Refactoring Demo

---
+++ demo/README.md
@@ -0,0 +1,7 @@
+# EV3Dev-lang-java / Demo
+
+This is the educational example to demostrate latest features added in 
+in the project.
+
+In this example, a simple robot which it detect obstacles on the front 
+and it tries to avoid the obstacle.
--- demo/src/main/java/examples/Demo.java
+++ demo/src/main/java/examples/Demo.java
@@ -1,39 +1,89 @@
 package examples;
 
+import ev3dev.actuators.motors.EV3LargeRegulatedMotor;
+import ev3dev.actuators.motors.EV3MediumRegulatedMotor;
+import ev3dev.sensors.Battery;
+import ev3dev.sensors.ev3.EV3GyroSensor;
 import ev3dev.sensors.slamtec.RPLidarA1;
 import ev3dev.sensors.slamtec.model.Scan;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 import lombok.extern.slf4j.Slf4j;
 
 public @Slf4j class Demo {
 
+    private static EV3LargeRegulatedMotor mA = null;
+    private static EV3MediumRegulatedMotor mB = null;
+
+    private static EV3GyroSensor gyroSensor = null;
+    private static Battery battery;
+    private static final float VOLTAGE_THRESHOLD = 8000;
+
+    private static final String USBPort = "/dev/ttyUSB0";
+    private static RPLidarA1 lidar = null;
+
     public static void main(String[] args) throws Exception {
 
-        log.info("Testing RPLidar on a EV3 Brick with Java");
-        final String USBPort = "/dev/ttyUSB0";
-        final RPLidarA1 lidar = new RPLidarA1(USBPort);
+        mA = new EV3LargeRegulatedMotor(MotorPort.A);
+        mB = new EV3MediumRegulatedMotor(MotorPort.B);
+        mA.setSpeed(500);
+        mB.setSpeed(500);
+
+        gyroSensor = new EV3GyroSensor(SensorPort.S1);
+        final SampleProvider sp = gyroSensor.getAngleAndRateMode();
+        battery = Battery.getInstance();
+        lidar = new RPLidarA1(USBPort);
         lidar.init();
 
-        for(int x = 0; x <= 10; x++){
+        while(battery.getVoltage() >= VOLTAGE_THRESHOLD) {
+
             final Scan scan = lidar.scan();
-            log.info("Iteration: {}, Measures: {}", x, scan.getDistances().size());
-            scan.getDistances()
-                .stream()
-                .filter(measure -> {
-                    if(
-                        (measure.getAngle() >= 345) ||
-                        (measure.getAngle() <=15)) {
+            long counter = scan.getDistances()
+                    .stream()
+                    .filter(measure -> {
+                        if((measure.getAngle() >= 345) || (measure.getAngle() <=15)) {
                             if(measure.getDistance() <= 50) {
                                 return true;
                             }
                             return false;
+                        }
+                        return false;
+                    })
+                    .count();
+
+            if(counter < 5){
+                mA.forward();
+                mB.forward();
+            }else{
+                mA.stop();
+                mB.stop();
+                mA.forward();
+
+                //Control loop
+                while(true){
+                    float [] sample = new float[sp.sampleSize()];
+                    sp.fetchSample(sample, 0);
+                    int value = (int)sample[0];
+
+                    log.info("Gyro angle/rate: {}", value);
+
+                    if(value >= 45){
+                        log.info("Rotated 45 degrees");
+                        mA.stop();
+                        break;
                     }
-                    return false;
-                })
-                .forEach(System.out::println);
+
+                }
+            }
+
         }
 
-        lidar.close();
-        log.info("End demo");
+        Delay.msDelay(2000);
+        mA.stop();
+        mB.stop();
         System.exit(0);
     }
+
 }
