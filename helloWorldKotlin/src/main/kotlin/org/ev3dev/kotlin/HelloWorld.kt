d943beeb88cb82d38a16f3f30228b6e0b21d236a
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-06-27 20:07:46 +0100

    Minor change in Kotlin example

--- helloWorldKotlin/src/main/kotlin/org/ev3dev/kotlin/HelloWorld.kt
+++ helloWorldKotlin/src/main/kotlin/org/ev3dev/kotlin/HelloWorld.kt
@@ -2,16 +2,7 @@ package org.ev3dev.kotlin
 
 import ev3dev.actuators.Sound
 
-//TODO At the moment, it is necessary to have a Main class to associate in the Manifest
-class HelloWorld {
-
-    companion object {
-
-        @JvmStatic
-        fun main(args: Array<String>) {
-            print("Hello World")
-            Sound.getInstance().beep()
-        }
-
-    }
+fun main(args: Array<String>) {
+    print("Hello World")
+    Sound.getInstance().beep()
 }
